f4e099026a2e3fc2dfbf823d584a9b7b1a5b27ff
Donny Velazquez <donnyv@gmail.com>
2013-06-28 23:21:55 -0400

    Create app controller and create app core project

---
+++ QueHacer/QueHacer.Core/AppLogic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueHacer.Core
+{
+    public class AppLogic
+    {
+        public static Tuple<bool, string> AddTask()
+        {
+            return new Tuple<bool, string>(true, "success");
+        }
+
+        public static string GetTodoDBjson()
+        {
+            return string.Empty;
+        }
+    }
+}
---
+++ QueHacer/QueHacer/Controllers/AppController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using QueHacer.Core;
+
+namespace QueHacer.Controllers
+{
+    public class AppController : Controller
+    {
+        
+        // GET: /App/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JavaScriptResult TodoDB(string id)
+        {
+            return (new JavaScriptResult() { Script = AppLogic.GetTodoDBjson() });
+        } 
+
+        [HttpPost]
+        public ContentResult AddTask()
+        {
+            object ret = null;
+            var ErrorMsg = string.Empty;
+            if (ret == null)
+                return JsonUtilities.Result(true, ErrorMsg).JsonContentResult;
+            else
+                return JsonUtilities.Result(ret).JsonContentResult; 
+        }
+    }
+}
