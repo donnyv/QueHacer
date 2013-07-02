20fa24ba17e6efad874b50ded2fc9a518d06fc70
Donny Velazquez <donnyv@gmail.com>
2013-07-02 15:05:57 -0400

    Clean up task card, fix share url bug

--- QueHacer/QueHacer.Core/AppLogic.cs
+++ QueHacer/QueHacer.Core/AppLogic.cs
@@ -135,5 +135,11 @@ namespace QueHacer.Core
             dbJSON += "todoDB.Tasks = " + ret.Json + ";";
             return dbJSON;
         }
+
+        private static readonly DateTime UnixEpoch = new DateTime(1970, 1, 1, 0, 0, 0,DateTimeKind.Utc);
+        public static DateTime FromMillisecondsSinceUnixEpoch(long milliseconds)
+        {
+            return UnixEpoch.AddMilliseconds(milliseconds);
+        }
     }
 }
